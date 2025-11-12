bbb824690f9581bff846e7d8128eb689c76ee188
Joan LUMIA <jlumia_ext@lemonway.com>
2025-11-12 10:12:31 +0100

    fix: Upgrade azurerm provider to 4.x to fix Logic App creation bug

--- project-demo-ipaas-by-ai/env/dev/main.tf
+++ project-demo-ipaas-by-ai/env/dev/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"  # Upgraded to 4.x to fix Logic App creation bug
     }
   }
 
