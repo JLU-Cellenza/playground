40d11bb55d7e2af756d895b37850ef888cfafbe2
Joan LUMIA <jlumia_ext@lemonway.com>
2025-11-12 10:04:44 +0100

    fix: Comment out all outputs referencing disabled modules

--- project-demo-ipaas-by-ai/env/dev/outputs.tf
+++ project-demo-ipaas-by-ai/env/dev/outputs.tf
@@ -1,41 +1,43 @@
 # env/dev/outputs.tf
+# MINIMAL OUTPUTS - Only for resources that are deployed
 
 output "resource_group_name" {
   description = "The name of the resource group"
   value       = azurerm_resource_group.this.name
 }
 
-output "log_analytics_workspace_id" {
-  description = "Log Analytics Workspace ID"
-  value       = module.log_analytics.workspace_id
-}
-
-output "app_insights_instrumentation_key" {
-  description = "Application Insights instrumentation key"
-  value       = module.app_insights.instrumentation_key
-  sensitive   = true
-}
-
-output "servicebus_namespace_name" {
-  description = "Service Bus namespace name"
-  value       = module.servicebus.namespace_name
-}
-
-output "servicebus_connection_string" {
-  description = "Service Bus connection string"
-  value       = module.servicebus.primary_connection_string
-  sensitive   = true
-}
-
-output "function_app_name" {
-  description = "Function App name"
-  value       = module.function_app.function_app_name
-}
-
-output "function_app_url" {
-  description = "Function App URL"
-  value       = "https://${module.function_app.default_hostname}"
-}
+# Temporarily commented out - not deployed in minimal config
+# output "log_analytics_workspace_id" {
+#   description = "Log Analytics Workspace ID"
+#   value       = module.log_analytics.workspace_id
+# }
+#
+# output "app_insights_instrumentation_key" {
+#   description = "Application Insights instrumentation key"
+#   value       = module.app_insights.instrumentation_key
+#   sensitive   = true
+# }
+#
+# output "servicebus_namespace_name" {
+#   description = "Service Bus namespace name"
+#   value       = module.servicebus.namespace_name
+# }
+#
+# output "servicebus_connection_string" {
+#   description = "Service Bus connection string"
+#   value       = module.servicebus.primary_connection_string
+#   sensitive   = true
+# }
+#
+# output "function_app_name" {
+#   description = "Function App name"
+#   value       = module.function_app.function_app_name
+# }
+#
+# output "function_app_url" {
+#   description = "Function App URL"
+#   value       = "https://${module.function_app.default_hostname}"
+# }
 
 output "logicapp_01_name" {
   description = "Logic App 01 name"
@@ -69,12 +71,13 @@ output "logicapp_01_url" {
 #   value       = module.apim.developer_portal_url
 # }
 
-output "storage_platform_name" {
-  description = "Platform storage account name"
-  value       = module.storage_platform.name
-}
-
-output "storage_platform_blob_endpoint" {
-  description = "Platform storage blob endpoint"
-  value       = module.storage_platform.primary_blob_endpoint
-}
+# Temporarily commented out - not deployed in minimal config
+# output "storage_platform_name" {
+#   description = "Platform storage account name"
+#   value       = module.storage_platform.name
+# }
+#
+# output "storage_platform_blob_endpoint" {
+#   description = "Platform storage blob endpoint"
+#   value       = module.storage_platform.primary_blob_endpoint
+# }
