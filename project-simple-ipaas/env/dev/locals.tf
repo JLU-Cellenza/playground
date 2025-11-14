8ff251551cbb1244a3491b9f6fbc274234029121
Joan LUMIA <jlumia_ext@lemonway.com>
2025-11-14 11:06:10 +0100

    fix: format terraform files in project-simple-ipaas

--- project-simple-ipaas/env/dev/locals.tf
+++ project-simple-ipaas/env/dev/locals.tf
@@ -7,19 +7,19 @@ locals {
 
   # Common tags applied to all resources
   common_tags = {
-    environment  = local.environment
-    project      = local.project
-    owner        = var.owner
-    cost_center  = var.cost_center
-    created_by   = "terraform"
-    managed_by   = "terraform"
+    environment = local.environment
+    project     = local.project
+    owner       = var.owner
+    cost_center = var.cost_center
+    created_by  = "terraform"
+    managed_by  = "terraform"
   }
 
   # Resource naming following pattern: <svc>-<env>-<org>-<project>-<purpose>-<instance>
-  resource_group_name     = "rg-${local.environment}-${local.org}-${local.project}"
-  servicebus_name         = "svb-${local.environment}-${local.org}-${local.project}-01"
-  keyvault_name           = "kv-${local.environment}-${local.org}-${local.project}-01"
-  storage_account_name    = "st${local.environment}${local.org}${local.project}01"
-  service_plan_name       = "asp-${local.environment}-${local.org}-${local.project}-01"
-  logic_app_name          = "loa-${local.environment}-${local.org}-${local.project}-01"
+  resource_group_name  = "rg-${local.environment}-${local.org}-${local.project}"
+  servicebus_name      = "svb-${local.environment}-${local.org}-${local.project}-01"
+  keyvault_name        = "kv-${local.environment}-${local.org}-${local.project}-01"
+  storage_account_name = "st${local.environment}${local.org}${local.project}01"
+  service_plan_name    = "asp-${local.environment}-${local.org}-${local.project}-01"
+  logic_app_name       = "loa-${local.environment}-${local.org}-${local.project}-01"
 }
